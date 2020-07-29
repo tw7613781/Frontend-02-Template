565485b25c016ade326c79d3c9ffb92abb02c51d
Max Tang <tw7613781@gmail.com>
2020-07-29 11:46:58 +0900

    feat: init week05 howework

---
+++ week05/httpServer/server.js
@@ -0,0 +1,32 @@
+const http = require('http')
+
+const file = `<html lang="en">
+<head>
+  <title>Document</title>
+</head>
+<body>
+  <div id="text">
+    <p>Hello World</p>
+    <img id="myid" />
+  </div>
+</body>
+</html>
+`
+
+http.createServer((request, response) => {
+  let body = []
+  console.log(request.headers)
+  request.on('error', (err) => {
+    console.error(err)
+  }).on('data', (chunk) => {
+    body.push(chunk)
+  }).on('end', () => {
+    body = Buffer.concat(body).toString()
+    console.log('body:', body)
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    response.write(file)
+    response.end()
+  })
+}).listen(8888)
+
+console.log('server started on port:8888')
