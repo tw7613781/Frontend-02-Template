a76b68546f12e35949bb0f3acb72796b4ac7f4c6
Tang Wei <tw7613781@gmail.com>
2020-10-20 10:09:19 +0900

    feat: add webpack-dev-server and refactor project to make a mini-framework

--- week12/jsx/main.js
+++ week12/jsx/main.js
@@ -1,71 +1,14 @@
-function createElement(type, attributes, ...children) {
-    let element
-    if(typeof type === "string")
-        element = new ElementWrapper(type)
-    else
-        element = new type
-    for(let name in attributes) {
-        element.setAttribute(name, attributes[name])
-    }
-    for(let child of children){
-        if(typeof child === "string") {
-            child = new TextWrapper(child)
-        }
-        element.appendChild(child)
-    }
-    return element
-}
+import { Component, createElement } from "./framework"
 
-class Div {
+class Carousel extends Component{
     constructor(){
-        this.root = document.createElement("div")
+        super()
     }
-    setAttribute(name, value) {
-        this.root.setAttribute(name, value)
-    }
-    appendChild(child) {
-        child.mountTo(this.root)
-    }
-    mountTo(parent) {
-        parent.appendChild(this.root)
+    render() {
+        return document.createElement('div')
     }
 }
 
-class ElementWrapper{
-    constructor(type){
-        this.root = document.createElement(type)
-    }
-    setAttribute(name, value) {
-        this.root.setAttribute(name, value)
-    }
-    appendChild(child) {
-        child.mountTo(this.root)
-    }
-    mountTo(parent) {
-        parent.appendChild(this.root)
-    }    
-}
+let a = <Carousel />
 
-class TextWrapper{
-    constructor(text){
-        this.root = document.createTextNode(text)
-    }
-    setAttribute(name, value) {
-        this.root.setAttribute(name, value)
-    }
-    appendChild(child) {
-        child.mountTo(this.root)
-    }
-    mountTo(parent) {
-        parent.appendChild(this.root)
-    }    
-}
-
-let a = <div id="a">
-        <span>a</span>
-        <span>b</span>
-        <span>c</span>
-    </div>
-
-// document.body.appendChild(a)
 a.mountTo(document.body)
